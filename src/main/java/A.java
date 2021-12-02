34987a94de15ca9a0ddedb0812b8a5dfeecc8ef1
zhengzn111 <95420256+zhengzn111@users.noreply.github.com>
2021-12-02 21:10:15 +0800

    Update A.java

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -6,5 +6,6 @@ public class A {
         System.out.println("hot ");
         System.out.println("master 2");
         System.out.println("hot2 ");
+        System.out.println("pull and push ");
     }
 }
